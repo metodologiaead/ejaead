d4d5e1d2e83d4d0146d59d7bbed1b98883cfb26f
bellmontsistema <bellmontsistema@gmail.com>
2021-04-27 12:07:38 -0300

    título mudado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import * as S from './styled'
+import React from "react"
+import * as S from "./styled"
 import Nav from "../Nav"
 
-import Modelo from '../../images/modelo.png'
-import Modal from '../Modal'
+import Modelo from "../../images/modelo.png"
+import Modal from "../Modal"
 
 const Header = () => {
-
-    return (
-       <S.Section>
-           <Nav />
-           <S.Container>
-
-               <S.InfoWrapper>
-                   <h2>EJA EAD ENSINO MÉDIO E FUNDAMENTAL<br/>
-                    <span>DE 1 A 6 MESES</span></h2>
-                   <p>Você pode matricular-se sem sair de casa. 
-                    Ao matricular-se no Centro de Ensino Educa 
-                    Nexus você recebe o Material online.
-                   </p>
-                   <S.LinksWrapper>
-                    <Modal />
-                    <S.WhtasButton href="https://api.whatsapp.com/send?phone=5582999762839" target="__blank">WhatsApp</S.WhtasButton>
-                   </S.LinksWrapper>
-               </S.InfoWrapper>
-               <S.ImageWrapper>
-                    <img src={Modelo} alt="Imagem Homem Saindo do Notebook" />
-               </S.ImageWrapper>
-
-           </S.Container>
-       </S.Section>
-    )
+  return (
+    <S.Section>
+      <Nav />
+      <S.Container>
+        <S.InfoWrapper>
+          <h2>
+            EJA EAD PROFISSIONALIZANTE
+            <br />
+            ENSINO MÉDIO E FUNDAMENTAL
+            <br />
+            <span>DE 1 A 6 MESES</span>
+          </h2>
+          <p>
+            Você pode matricular-se sem sair de casa. Ao matricular-se no Centro
+            de Ensino Educa Nexus você recebe o Material online.
+          </p>
+          <S.LinksWrapper>
+            <Modal />
+            <S.WhtasButton
+              href="https://api.whatsapp.com/send?phone=5582999762839"
+              target="__blank"
+            >
+              WhatsApp
+            </S.WhtasButton>
+          </S.LinksWrapper>
+        </S.InfoWrapper>
+        <S.ImageWrapper>
+          <img src={Modelo} alt="Imagem Homem Saindo do Notebook" />
+        </S.ImageWrapper>
+      </S.Container>
+    </S.Section>
+  )
 }
 
 export default Header
