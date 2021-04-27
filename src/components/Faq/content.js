e369f505ceae027e63822f78a2478c016a37b914
bellmontsistema <bellmontsistema@gmail.com>
2021-04-27 11:59:21 -0300

    refeito o faq

---
+++ src/components/Faq/content.js
@@ -0,0 +1,179 @@
+export const itemsFaq = [
+  {
+    id: "o-que-e-o-eja-ead",
+    title: "O que é o EJA-EAD?",
+    description:
+      "EAD é a sigla para Educação À Distância. É uma modalidade de ensino que dá mais flexibilidade e autonomia, pois não se exige do aluno a presença regular em um espaço físico determinado. Vem ganhando muito espaço por acompanhar as evoluções tecnológicas, a possibilidade de fazer seus próprios horários e com preços mais acessíveis que a modalidade de ensino presencial.",
+  },
+  {
+    id: "quais-sao-as-vantagens-da-educacao-a-distancia",
+    title: "Quais são as vantagens da educação à distância?",
+    description:
+      "As principais vantagens da EAD são a flexibilidade de horários e a autonomia de estudos. Você pode dedicar-se nos horários que considerar mais adequados e conciliar melhor com outras atividades do cotidiano. Além disso, do ponto de vista financeiro os preços da educação a distância são mais acessíveis que a de escolas presenciais, além de diminuir custos diários com transporte, por exemplo.",
+  },
+  {
+    id: "quem-pode-matricular-se",
+    title: "Quem pode matricular-se?",
+    description:
+      "Alunos de todo o Brasil. Desde que obedeçam ao pré-requisito da idade: 18 anos completos para o Ensino Médio (antigo 2º grau). Não é obrigatório prévio histórico escolar.",
+  },
+  {
+    id: "existe-idade-maxima-para-poder-fazer-o-eja-ead-profissionalizante",
+    title: "Existe idade máxima para poder fazer o EJA EAD PROFISSIONALIZANTE?",
+    description:
+      "A idade máxima não existe e se uma pessoa de 100 anos de idade quiser fazer o EJA EAD PROFISSIONALIZANTE, poderá fazer sem problemas. A educação é aberta a todos e não pode privar pessoas do acesso ao conhecimento e ao ensino. Sempre haverá uma forma de estudar e manter-se atualizado para o exercício da cidadania e o gozo aos direitos como cidadão.",
+  },
+  {
+    id: "qual-e-a-relacao-entre-curso-supletivo-e-eja",
+    title: "Qual é a relação entre curso supletivo e EJA?",
+    description:
+      "O curso supletivo se refere a uma nomenclatura antiga e deixou de existir com esse nome, agora se chama Curso de Educação para Jovens e Adultos (EJA).",
+  },
+  {
+    id: "como-fazer-o-eja-ead-profissionalizante",
+    title: "Como fazer o EJA EAD PROFISSIONALIZANTE?",
+    description:
+      "Existem diferentes formas de realizar o EJA EAD PROFISSIONALIZANTE, mas a forma mais rápida e prática é estudar em casa todo o conteúdo que é cobrado na prova, e quando se sentir preparado para isso deverá procurar em sua cidade o local de realização da Prova.",
+  },
+  {
+    id: "e-possivel-terminar-meus-estudos-online",
+    title: "É possível terminar meus estudos online?",
+    description:
+      "Em nosso Portal de Ensino o aluno matriculado terá acesso a um material completo que irá ajudá-lo a se preparar para as provas. Quando o aluno se sentir preparado, poderá realizar a Prova do EJA EAD PROFISSIONALIZANTE.",
+  },
+  {
+    id:
+      "quem-faz-eja-ead-profissionalizante-pode-fazer-o-exame-nacional-do-ensino-medio-enem-e-faculdade",
+    title:
+      "Quem faz EJA EAD Profissionalizante pode fazer o Exame Nacional do Ensino Médio (ENEM) e faculdade?",
+    description:
+      "Sim. Concluindo o Ensino Médio EJA – EAD Profissionalizante, você receberá o certificado de conclusão com a mesma validade de uma escola regular.",
+  },
+  {
+    id:
+      "quem-faz-eja-ead-profissionalizante-tem-dificuldades-em-encontrar-emprego",
+    title:
+      "Quem faz EJA EAD Profissionalizante tem dificuldades em encontrar emprego?",
+    description:
+      "Não. Uma empresa não pode negar um posto de trabalho a uma pessoa alegando que sua formação fundamental e média tenha sido por curso EJA EAD Profissionalizante.",
+  },
+  {
+    id: "eja-ead-e-mais-facil-que-o-ensino-presencial",
+    title: "EJA EAD é mais fácil que o ensino presencial?",
+    description:
+      "Não. Assim como a EJA presencial, a modalidade a distância também tem provas, trabalhos e notas mínimas para receber o certificado de conclusão do ensino médio. Também exige tempo, dedicação e objetividade. No entanto, o EAD oferece mais liberdade para estudar nos seus próprios horários, tendo um ritmo diferente do tradicional.",
+  },
+  {
+    id: "quais-as-formas-de-pagamento",
+    title: "Quais as formas de pagamento?",
+    description: `Para dar mais flexibilidade, a Instituição oferece ao aluno as seguintes formas de pagamento: • Dinheiro • Boleto Bancário • Cartão de crédito • Cartão de débito`,
+  },
+  {
+    id:
+      "existe-um-numero-de-credenciamento-ou-autorizacao-do-mec-ministerio-da-educacao",
+    title:
+      "Existe um número de credenciamento ou autorização do MEC (Ministério da Educação)?",
+    description:
+      "Não. As instituições de Ensino Fundamental (antigo 1º grau) e Ensino Médio (antigo 2º grau) não são autorizadas pelo MEC (Ministério da Educação), mas sim pelas secretarias estaduais de educação.",
+  },
+  {
+    id:
+      "qual-e-a-validade-do-certificado-do-curso-da-na-modalidade-ejaensino-medio-ead",
+    title:
+      "Qual é a validade do certificado do curso da na modalidade EJA/Ensino Médio EAD?",
+    description:
+      "O certificado de conclusão do curso da EJA/Ensino Médio emitido para quem estuda na modalidade EAD têm a mesma validade que o certificado emitido por escolas presenciais. O Ministério da Educação dá o mesmo valor a ambos os documentos, conforme a Lei de Diretrizes e Bases da Educação (LDB), que desde 1996 determina que os certificados dos cursos a distância tenham o mesmo valor dos presenciais.",
+  },
+  {
+    id: "como-confirmo-a-autenticidade-do-meu-certificado",
+    title: "Como confirmo a autenticidade do meu certificado?",
+    description:
+      "A confirmação do certificado se dá através da publicação do nome do aluno no diário oficial e também através de confirmação na secretaria escolar através do número (82) 98719-5545 e por e-mail eadmetodologia@gmail.com.",
+  },
+  {
+    id: "posso-fazer-concurso-publico",
+    title: "Posso fazer concurso público?",
+    description:
+      "Após concluir o ensino médio (antigo segundo grau), o estudante pode se inscrever em concursos que solicitam o ensino médio completo, pode se matricular em um curso superior, além de ter muito mais chances no mercado de trabalho.",
+  },
+  {
+    id:
+      "eu-tenho-restricoes-em-meu-nome-spcserasa-posso-fazer-boleto-bancario-ou-carne-mesmo-assim",
+    title:
+      "Eu tenho restrições em meu nome (SPC/SERASA), posso fazer boleto bancário ou carnê mesmo assim?",
+    description:
+      "Sim, o Centro Educa Nexus não consulta nomes, mas o prazo para a entrega do Histórico Escolar só começa a ser contado a partir da inexistência de prestações a receber.",
+  },
+  {
+    id: "quais-documentos-sao-necessarios-para-fazer-a-matricula",
+    title: "Quais documentos são necessários para fazer a matrícula?",
+    description: `• RG e CPF (não pode ser carteira de habilitação); 
+    • 1 foto 3×4;
+    • Comprovante de residência.
+    No ato da matrícula, RG e CPF são suficientes para dar início ao curso, os outros documentos podem ser entregues posteriormente. Mas lembramos: o prazo para entrega do histórico pode ficar comprometido se não forem entregues até a data da aprovação
+    `,
+  },
+  {
+    id: "e-se-eu-nao-for-aprovado",
+    title: "E se eu não for aprovado?",
+    description:
+      "O aluno que não alcançar média 5,0 nas provas finais, poderá refazer a prova sem nenhuma taxa adicional.",
+  },
+  {
+    id: "qual-o-prazo-para-receber-meu-historico-escolar",
+    title: "Qual o prazo para receber meu Histórico Escolar?",
+    description:
+      "O prazo é de 45 dias úteis após a aprovação. Isso se não houver prestações a serem pagas ou a falta da documentação exigida",
+  },
+  {
+    id: "quais-as-legislacoes-que-autorizam-este-tipo-de-ensino",
+    title: "Quais as legislações que autorizam este tipo de ensino?",
+    description: `Seguem as seguintes legislações:
+    • Lei nº 9.394/96
+    • Resolução CNE/CEB nº 01/00
+    • Resolução CNE/CEB nº 03/10
+    • Resolução CEE/PB 200/2018
+    • Resolução CEE/PB 043/2020
+    • Resolução CEE/PB 044/2020`,
+  },
+  {
+    id: "alunos-emancipados-podem-fazer-o-eja",
+    title: "Alunos emancipados podem fazer o EJA?",
+    description: "Não, somente alunos com 18 anos completos;",
+  },
+  {
+    id:
+      "sim-a-unidade-consegue-emitir-uma-declaracao-de-conclusao-e-a-mesma-pode-ser-apresentada-em-outras-instituicoes-ou-oportunidades-de-emprego",
+    title:
+      "Após concluir é possível a emissão de algum documento que informe a conclusão do EJA-EAD PROFSSIONALIZANTE?",
+    description:
+      "Sim, a unidade consegue emitir uma declaração de conclusão e a mesma pode ser apresentada em outras instituições ou oportunidades de emprego",
+  },
+  {
+    id: "quais-os-beneficios-de-cursar-o-eja-ead-profissionalizante",
+    title: "Quais os benefícios de cursar o EJA-EAD PROFISSIONALIZANTE?",
+    description: `– Ao concluir o ensino médio (antigo 2° grau) o concluinte sai na frente dos demais indivíduos em busca de uma vaga no mercado de trabalho.
+
+    – Dá oportunidade de alavancar seu currículo ingressando em outras formações.`,
+  },
+  {
+    id: "aprender-no-seu-ritmo",
+    title: "Aprender no seu ritmo:",
+    description:
+      "Diferente da modalidade presencial onde os alunos assistem aulas em turmas com uma certa quantidade de alunos.",
+  },
+  {
+    id:
+      "caso-necessario-e-possivel-o-reconhecimento-da-assinatura-presentes-no-certificado",
+    title:
+      "Caso necessário é possível o reconhecimento da assinatura presentes no certificado?",
+    description:
+      "Sim, dependendo da necessidade é possível o reconhecimento das assinaturas",
+  },
+  {
+    id: "qual-o-endereco-sede-da-matriz-centro-educa-nexus",
+    title: "Qual o endereço sede da Matriz Centro Educa Nexus?",
+    description:
+      "Nossa instituição está localizada na Av. Piauí, n° 75 – Estados – João Pessoa – PB",
+  },
+]
